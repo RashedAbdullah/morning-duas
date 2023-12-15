84a9e3b199b98d4f49a4cebecb6c7c86aa485370
rashedabdullah <maarashed40@gmail.com>
2023-12-15 08:19:23 +0600

    morning masnun duas after salat

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MorningDua from "./DuaJSX/MorningDua";
+
+
+function App() {
+  return (
+    <div>
+      <MorningDua />
+    </div>
+  )
+}
+
+export default App
---
+++ src/DuaJSX/MorningDua.jsx
@@ -0,0 +1,40 @@
+import duas from '../duasJSON/duas.json';
+import '../css/duas.css'
+import { useState } from 'react';
+
+const MorningDua = () => {
+
+    const [isCompleted, setIsCompleted] = useState(false);
+
+   
+    const handleCheckbox = (e, id)=>{
+        const filteddua = duas.filter((dua)=>dua.id === id);
+        setIsCompleted(e.target.checked)
+        console.log(filteddua)
+    }
+
+  return (
+    <div dir='rtl' className='mainDuaDiv'>
+        <div className='duaTitle'><h2>أذكار الصباح والمساء</h2></div>
+        {
+            duas.map(dua=>(
+                <div key={dua.id} className={isCompleted? "markingCompleted" : ""}>
+                    <div>
+                        <span className='duaNumber'>{dua.id} </span>
+                        <span className='singleDuaTitle'>{dua.title} </span>
+                        <span >{dua.dua} </span>
+                        <span className='duaRepeat'>{dua.repeat} </span>
+                        <span className='duaReference'>( {dua.reference} )</span>
+                    </div>
+                </div>
+            ))
+        }
+        <div className='duafooter'>
+            <p>(من حصن الإسلام من أذكار الكتاب والسنة بعد مراجعة المراجع)</p>
+            <p>اللهم وفقنا لطاعتك واتباع سنة حبيبك ةواجعلنا من الصالحين</p>
+        </div>
+    </div>
+  )
+}
+
+export default MorningDua;
